87da461ef4f4045d0a078766e538707ea3d2bcab
PyaeLinAung9 <133384381+PyaeLinAung9@users.noreply.github.com>
2023-09-23 14:38:44 +0630

    log-viewer insall

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,32 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Utility;
 use App\ReturnMessage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\Category\CategoryStoreRequest;
 use App\Repository\Category\CategoryRepositoryInterface;
+use Exception;
 
 class CategoryController extends Controller
 {
     private $categoryRepository;
 
     public function __construct(CategoryRepositoryInterface $categoryRepository) {
+        DB::connection()->enableQueryLog();
         $this->categoryRepository = $categoryRepository;
     }
 
     public function categoryForm() {
-
         return view('backend.category.form');
     }
 
     public function categoryCreate(CategoryStoreRequest $request) {
-        $result     = $this->categoryRepository->create($request->all());
-        if($result["softGuideStatusCode"] == ReturnMessage::OK) {
-            return redirect()->route('categoryList')->with('successMsg','category create success.');
-        }else{
-            return redirect()->route('categoryList')->with('errorMsg','category create fail.');
+        try{
+            $result     = $this->categoryRepository->create($request->all());
+            $logs = "Category Create Screen::";
+            Utility::saveDebugLog($logs);
+            if($result["softGuideStatusCode"] == ReturnMessage::OK) {
+                return redirect()->route('categoryList')->with('successMsg','category create success.');
+            }else{
+                return redirect()->route('categoryList')->with('errorMsg','category create fail.');
+            }
+        }catch(Exception $e){
+            Utility::saveErrorLog($logs);
+            abort(500);
         }
-        return true;
     }
 
     public function categoryList() {
